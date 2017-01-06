09ab0a6c13f6f66dde4b79a0d446ca52abe5e893
shahmir811 <shahmirkj@gmail.com>
2017-01-06 08:47:59 +0500

    Start Project

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+
+class RegisterController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+
+    }
+}
