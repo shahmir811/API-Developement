a59271dd18bfc725f72af391c106a09cbb6662d5
shahmir811 <shahmirkj@gmail.com>
2017-01-07 09:06:09 +0500

    8-Topic Index

--- app/Http/Controllers/TopicController.php
+++ app/Http/Controllers/TopicController.php
@@ -7,9 +7,23 @@ use App\Post;
 use Illuminate\Http\Request;
 use App\Transformers\TopicTransformer;
 use App\Http\Requests\StoreTopicRequest;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 class TopicController extends Controller
 {
+    public function index()
+    {
+      $topics = Topic::latestFirst()->paginate(3);
+      $topicCollection = $topics->getCollection();
+
+      return fractal()
+            ->collection($topics)
+            ->parseIncludes(['user'])
+            ->transformWith(new TopicTransformer)
+            ->paginateWith(new IlluminatePaginatorAdapter($topics))
+            ->toArray();
+    }
+
     public function store(StoreTopicRequest $request)
     {
         $topic = new Topic;
